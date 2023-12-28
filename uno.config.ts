0ed16010a3861710be1e414c8e848ccc2f7c83c9
LoTwT <709937065@qq.com>
2023-12-28 17:23:59 +0800

    chore: init

---
+++ uno.config.ts
@@ -0,0 +1,31 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss"
+
+export default defineConfig({
+  shortcuts: [],
+  safelist: [],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+    }),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        sans: "DM Sans",
+        serif: "DM Serif Display",
+        mono: "DM Mono",
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+})
